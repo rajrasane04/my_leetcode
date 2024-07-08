c65aaf46ee7491e5c58792427d741ac20e222794
Raj Rasane <154857460+rajrasane04@users.noreply.github.com>
2024-07-08 21:33:38 +0530

    Time: 35 ms (78.46%) | Memory: 16.6 MB (49.47%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,4 +1,6 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        return (self.findTheWinner(n-1, k)+(k-1))%n+1 if n>1 else 1
-        
+        if n == 1:
+            return 1
+        else:
+            return (self.findTheWinner(n-1, k)+k-1) % n + 1
